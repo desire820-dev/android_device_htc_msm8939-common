7bea2cd55f1918037aa72bd0b70ff3643a9921e9
xiaoxiao <xiaoxiao6208@gmail.com>
2016-07-09 01:59:18 +0800

    update Modem firmware

--- Android.mk
+++ Android.mk
@@ -55,9 +55,8 @@ ALL_DEFAULT_INSTALLED_MODULES += $(MBA_VENDOR_SYMLINKS)
 
 MODEM_IMAGES := \
     modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
-    modem.b06 modem.b07 modem.b08 modem.b10 modem.b12 modem.b15 \
-    modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 modem.b23 \
-    modem.b24 modem.b27 modem.b28 modem.mdt
+    modem.b08 modem.b10 modem.b14 modem.b15 modem.b16 modem.b17 \
+    modem.b18 modem.b19 modem.b22 modem.b23 modem.b25 modem.mdt
 
 MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MODEM_IMAGES)))
 $(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
